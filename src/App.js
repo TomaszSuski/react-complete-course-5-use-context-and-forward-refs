4ff6443683fb14f923fb37d66e23103121286ab9
Tomasz Suski <tomasz.suski@orba.co>
2023-03-07 10:05:01 +0100

    added useContext hook instead of consumer

--- src/App.js
+++ src/App.js
@@ -27,13 +27,18 @@ function App() {
   };
 
   return (
-      <AuthContext.Provider value={{isLoggedIn: isLoggedIn}}>
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+      }}
+    >
       <MainHeader onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-      </AuthContext.Provider>
+    </AuthContext.Provider>
   );
 }
 
